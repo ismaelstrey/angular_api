2053d244d0a468315d95d3d22d992ad43a59d85f
ismael strey <ismaelstrey@hotmail.com>
2019-05-30 17:59:23 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LoginComponent } from "./user/login/login.component";
+import { HomeComponent } from "./home/home/home.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "/", component: HomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './user/login/login.component';
+import { CadastroComponent } from './user/cadastro/cadastro.component';
+import { AtualizarComponent } from './user/atualizar/atualizar.component';
+import { NavbarComponent } from './header/navbar/navbar.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { EquipeComponent } from './empresa/equipe/equipe.component';
+import { HomeEmpresaComponent } from './home/home-empresa/home-empresa.component';
+import { HomeComponent } from './home/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent,
+    AtualizarComponent,
+    NavbarComponent,
+    FooterComponent,
+    EquipeComponent,
+    HomeEmpresaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
